790add67276add531f2fc4cef56e87299ad78a10
Ayush Kumar <ayushyadavjee9001@gmail.com>
2025-01-16 01:50:38 +0530

    IF ELSE PROBLEM

---
+++ _03_ifElse.java
@@ -0,0 +1,14 @@
+public class _03_ifElse {
+    public static void main(String[] args) {
+        int a , b ;
+        a = 1110 ;
+        b  = 115 ;
+        if(a>b){
+            System.out.println(" A is greater than  B ");
+        }
+        else{
+            System.out.println("A is smaller than B ");
+        }
+
+}
+}
