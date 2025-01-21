f58519f0a4a14c29a6a7472a2ac527c0ad62ff54
Ayush Kumar <ayushyadavjee9001@gmail.com>
2025-01-21 16:53:36 +0530

    File added

---
+++ work.java
@@ -0,0 +1,13 @@
+// import java.util.Scanner;
+class work{
+    public static void main(String[] args){
+        int i=100;
+        long l = i ;
+        float f = l;
+        System.out.println(f);
+    
+
+
+
+    }
+}
