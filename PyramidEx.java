8bad8f364017bc26e44d7dee5a9a6f56c8f087f8
Ayush Kumar <ayushyadavjee9001@gmail.com>
2025-01-31 14:52:12 +0530

    Pyramid

---
+++ PyramidEx.java
@@ -0,0 +1,10 @@
+public class PyramidEx {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) { // Corrected condition: `j <= i`
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
