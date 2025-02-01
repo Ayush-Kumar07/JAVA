2bdb7b3ab04fff70b6fd53e4c3c22b0ccf68c34b
Ayush Kumar <ayushyadavjee9001@gmail.com>
2025-02-01 23:42:28 +0530

    file added

---
+++ unit2/reversePyramid.java
@@ -0,0 +1,8 @@
+public class reversePyramid {
+    public static void main(String[] args) {
+        for(int i = 0; i<=5 ; i++){
+            for(int j =5; j>=i ; j--){
+                System.out.print("* ");
+            }
+            System.err.println();
+        }
