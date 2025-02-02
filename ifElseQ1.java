7d760479d0701b55eb35f0b3645d370172386c4a
Ayush Kumar <ayushyadavjee9001@gmail.com>
2025-02-02 23:56:44 +0530

    File added

---
+++ ifElseQ1.java
@@ -0,0 +1,13 @@
+public class ifElseQ1 {
+    public static void main(String[] args) {
+        int a , b ;
+        a = 110 ;
+        b  = 115 ;
+        if(a>b){
+            System.out.println(" A is greater than  B ");
+        }
+        else{
+            System.out.println("A is smaller than B ");
+        }
+    }
+}
