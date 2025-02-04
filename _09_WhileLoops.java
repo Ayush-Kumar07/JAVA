ff4d4d00168d724750d1f071d2ba18d8d413c184
Ayush Kumar <ayushyadavjee9001@gmail.com>
2025-02-05 00:23:31 +0530

    file added

---
+++ _09_WhileLoops.java
@@ -0,0 +1,15 @@
+public class _09_WhileLoops {
+    public static void main(String[] args) {
+        // int n = 1;
+        // while(n<=100){
+        //     System.out.println(n);
+        //     n++;
+        // }
+        int n=1 ;
+        while(n<=50){
+            System.out.println(n);
+            // n++;
+        }
+    }
+}
+ 
