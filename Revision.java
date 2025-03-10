d9a0dfb3f6f82a60ea550ecd9936042316afaafa
Ayush Kumar <ayushyadavjee9001@gmail.com>
2025-03-10 05:55:02 +0530

    Using Functions

--- Revision.java
+++ Revision.java
@@ -2,27 +2,14 @@ import java.util.Scanner;
 
 public class Revision {
     public static void main(String[] args) {
-        Scanner ab = new Scanner(System.in);
-        
-        System.out.print("Enter Concert Name: ");
-        String a = ab.nextLine();
-        
-        System.out.print("Enter Seat Number: ");
-        int c = ab.nextInt();
-        ab.nextLine(); // Consume newline
+       Scanner ab = new Scanner(System.in);
+        double a = ab.nextDouble();
+       double b = ab.nextDouble();
+      double c = (a+b)/2;
+      System.out.printf("The value is: %.2f ",c);
 
-        System.out.print("Enter Ticket Price: ");
-        double d = ab.nextDouble();
-        ab.nextLine(); // Consume newline
 
-        System.out.print("Is Available (true/false): ");
-        boolean k = ab.nextBoolean();
 
-        System.out.println("\nConcert Name: " + a);
-        System.out.println("Seat Number: " + c);
-        System.out.println("Ticket Price: " + d);
-        System.out.println("Available: " + k);
-        
-        ab.close(); // Close Scanner to avoid resource leak
+
     }
 }
