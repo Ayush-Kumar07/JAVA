a0308683e6a1c6132b21d83d25b8b9d153c59f6c
Ayush Kumar <ayushyadavjee9001@gmail.com>
2025-03-11 05:13:45 +0530

    file added

---
+++ ArrayAC.java
@@ -0,0 +1,12 @@
+public class ArrayAC {
+    public static void main(String[] args) {
+        
+        int[] marks= new int[3];
+        marks[0] = 94;
+        marks[1] = 97;
+        marks[2] = 96;
+        for(int i = 0 ; i<3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
